c50955e6c0de585de73f40999f79dfdc3c9e2a43
erichen886 <erichen886@gmail.com>
2020-06-08 11:18:31 -0700

    grabs id on click, switches between x and o

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,1 +1,13 @@
 console.log ('hello from app!')
+var player = ['X', 'O'];
+var currentPlayer = player[0];
+
+function changeContent(id) {
+  document.getElementById(id).innerHTML = currentPlayer;
+  document.getElementById(id).style = "Color: red";
+  if(currentPlayer === player[0]) {
+    currentPlayer = player[1];
+  } else {
+    currentPlayer = player[0]
+  }
+}
