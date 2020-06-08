2e240c7ed198116ccae1938a8ea31734dda8a45e
erichen886 <erichen886@gmail.com>
2020-06-08 11:36:03 -0700

    added player choices using array,switch to object

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,13 +1,25 @@
 console.log ('hello from app!')
 var player = ['X', 'O'];
 var currentPlayer = player[0];
+var playerX =[];
+var playerO =[];
 
+//getElementsByClassName returns an array
 function changeContent(id) {
   document.getElementById(id).innerHTML = currentPlayer;
   document.getElementById(id).style = "Color: red";
   if(currentPlayer === player[0]) {
     currentPlayer = player[1];
+    playerX.push(id);
+    console.log("playerX: ", playerX);
   } else {
     currentPlayer = player[0]
+    playerO.push(id);
+    console.log("playerO", playerO);
   }
+}
+
+function checkWinner() {
+
+
 }
